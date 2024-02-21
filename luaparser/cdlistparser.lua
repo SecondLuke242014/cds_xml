33ff02b72bd16b1d2383cfdb3c36967ea5df53b8
LukeOfficial <e87624273@gmail.com>
2024-02-21 12:15:25 +0400

    Update cdlistparser.lua

--- luaparser/cdlistparser.lua
+++ luaparser/cdlistparser.lua
@@ -1,1 +1,13 @@
+-- modulescript
+-- for luau
 
+local http = game:GetService("HttpService")
+
+local parser = {}
+
+function parser.parseCDList(xml:string)
+  local getRequest = http:GetAsync(xml)
+  if not string.find(getRequest,"cdlist") then
+    error()
+  end
+end
